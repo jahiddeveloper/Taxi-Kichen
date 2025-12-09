0078a4ace4dcb9f17aea120f0161629e281b9e86
jahiddeveloper <md.jahidulislam.mar@gmail.com>
2025-12-09 12:17:17 +0600

    Card loading and showing is compleat

--- src/Component/OrderContainer.jsx
+++ src/Component/OrderContainer.jsx
@@ -1,5 +1,6 @@
 import React, { use } from "react";
 import States from "./States";
+import OrderCard from "./Cards/OrderCard";
 
 const OrderContainer = ({ loadOrder }) => {
   let order = use(loadOrder);
@@ -9,7 +10,14 @@ const OrderContainer = ({ loadOrder }) => {
       <States></States>
 
       <section className="w-11/12 mx-auto py-5 grid grid-cols-1 lg:grid-cols-12 gap-5">
-        <div className="col-span-8">Orders</div>
+        <div className="col-span-8">
+          <h2 className="font-bold text-3xl">Current Order</h2>
+          <div className="space-y-5 mt-5">
+            {order.map((item) => (
+              <OrderCard key={item.id} order={item}></OrderCard>
+            ))}
+          </div>
+        </div>
 
         <div className="col-span-4">Cocking + Ready food</div>
       </section>
